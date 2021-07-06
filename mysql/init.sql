981005faed72d10c0a96d8aee1a2acb4d55e048d
J-Wilson07 <j.roderick.wilson07@gmail.com>
2021-07-06 19:56:27 +0100

    Database scripts changed.

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,1 +1,10 @@
-CREATE DATABASE [IF NOT EXISTS] issue_tracker;
+CREATE DATABASE IF NOT EXISTS issue_tracker;
+
+USE issue_tracker;
+
+CREATE TABLE IF NOT EXISTS usernames_and_passwords(
+	Username varchar(255),
+	Password varchar(255)
+);
+
+INSERT INTO usernames_and_passwords (Username, Password) VALUES ('Username', 'Password')
