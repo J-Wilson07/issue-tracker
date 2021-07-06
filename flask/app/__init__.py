6c0512d96c456ee3d9a9c269eba975a8e17134a7
J-Wilson07 <j.roderick.wilson07@gmail.com>
2021-07-06 02:37:43 +0100

    SQL stuff added.

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 from app import views
+
+@app.route("/login", methods = ["POST"])
+def home():
+    username = request.form['Username']
+    password = request.form['Password']
+
+    return render_template('login.html')
