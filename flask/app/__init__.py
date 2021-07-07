c1276a0ec2789ecb616e755f2b07378f0f7e961d
J-Wilson07 <j.roderick.wilson07@gmail.com>
2021-07-07 02:41:08 +0100

    Added more flask functionality.

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-from app import views
+#from app import views
 
-@app.route("/login", methods = ["POST"])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/home')
 def home():
-    username = request.form['Username']
-    password = request.form['Password']
-
-    return render_template('login.html')
+    return render_template('home.html')
